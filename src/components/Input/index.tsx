1e3ec85212297b0e88775fd8c957c5969cca28ae
Gabriel Andrade <gabriel.santana@gmail.com>
2021-05-17 05:51:32 -0300

    Conclusão componente Input

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -7,16 +7,20 @@ import {
 
 import { useField } from '@unform/core';
 
+
 import { Container } from './styles';
+
+
+
 
 interface InputProps{
   name:string;
-  icon:string;
+  icon:React.ComponentType<any>;
   params:string[]
 }
 
 export const Input = ({ name, icon: Icon, ...rest }:InputProps) => {
-  const inputRef = useRef(null);
+  const inputRef = useRef<HTMLInputElement>(null);
 
   const [isFocused, setIsFocused] = useState(false);
   const [isFilled, setIsFilled] = useState(false);
